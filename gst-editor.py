b720417d8afa27154054fdd8315c8de4ac619b47
Brandon Lewis <brandon_lewis@berkeley.edu>
2009-02-24 18:25:55 -0800

    application runs

--- gst-editor.py
+++ gst-editor.py
@@ -1,20 +1,15 @@
+#!/usr/bin/env python
 import gobject
 gobject.threads_init()
-import goocanvas
-import controller
-import view
 import gtk
 import os.path
-import pango
 import gst
-from receiver import receiver, handler
 from pipeline import PipelineView
 
 if __name__ == "__main__":
     w = gtk.Window()
-    n = gtk.Notebook()
     p = PipelineView(gst.Pipeline())
-    w.add(n)
+    w.add(p)
     w.connect("destroy", gtk.main_quit)
     w.show_all()
     gtk.main()
